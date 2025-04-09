a31c66e8c1b0f130c8c25026969048a0600a4b8e
YacqubAdan <yacquubadan.1@gmail.com>
2025-04-09 22:25:09 +0100

    add: route53 module to root main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,18 +19,27 @@ module "alb" {
 }
 
 module "ecs" {
-    source = "./modules/ecs"
-    vpc_id = module.vpc.vpc_id
-    ecs_sg_id = module.vpc.ecs_sg_id
-    alb_sg_id = module.vpc.alb_sg_id
-    http_listen_id = module.alb.http_listen_id
-    subnet_ids = module.vpc.public_subnet_id
-    tg_arn = module.alb.tg_arn
-    container_img = "582139844732.dkr.ecr.eu-west-2.amazonaws.com/delta-videos"
-    desired_count = 1
-    container_port = 3000 
-    host_port = 3000
-    memory = "4096"
-    cpu = "2048" 
+  source         = "./modules/ecs"
+  vpc_id         = module.vpc.vpc_id
+  ecs_sg_id      = module.vpc.ecs_sg_id
+  alb_sg_id      = module.vpc.alb_sg_id
+  http_listen_id = module.alb.http_listen_id
+  subnet_ids     = module.vpc.public_subnet_id
+  tg_arn         = module.alb.tg_arn
+  container_img  = "582139844732.dkr.ecr.eu-west-2.amazonaws.com/delta-videos"
+  desired_count  = 1
+  container_port = 3000
+  host_port      = 3000
+  memory         = "4096"
+  cpu            = "2048"
 }
+
+module "route53" {
+  source         = "./modules/route53"
+  dns_name       = "deltatv.yacquub.com"
+  alb_zone_id    = module.alb.alb_zone_id
+  alb_dns        = module.alb.alb_dns
+  dns_record_ttl = 3000
+}
+
 
